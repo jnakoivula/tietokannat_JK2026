610e0dd3cb2f540076c0a415460f5a96f9408d2d
jnakoivula <koivula.joonas95@gmail.com>
2026-03-08 18:35:41 +0200

    CRUD operations for book & borrower

---
+++ JS/config/database.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'library'
+});
+
+connection.connect(function(err){
+    if(err){
+        console.log(err);
+    } else {
+        console.log("Connected to MySQL Database");
+    }
+});
+
+module.exports = connection;
--- JS/routes/api.js
+++ JS/routes/api.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../config/database');
+
 //testing stuff
 router.get('/test', function(req, res) {
     res.json({
@@ -35,5 +37,129 @@ router.post('/example', function(request, response){
     console.log(request.body);
 });
 
+//GET all books
+router.get('/books', function(req, res){
+    db.query("SELECT * FROM book", function(err, result){
+        if(err){
+            res.send(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+//POST, add new book
+router.post('/books', function(req, res){
+    const { name, author, isbn } = req.body;
+    db.query(
+        "INSERT INTO book (name, author, isbn) VALUES (?, ?, ?)", [name, author, isbn],
+        function(err, result){
+            if(err){
+                res.send(err);
+            } else {
+                res.json({
+                    message: "Book added successfully", id: result.insertId
+                });
+            }
+        }
+    );
+});
+
+//UPDATE a book
+router.put('/books/:id', function(req, res){
+    const id = req.params.id;
+    const { name, author, isbn } = req.body;
+    db.query("UPDATE book SET name =?, author=?, isbn=? WHERE id_book=?", [name, author, isbn, id],
+        function(err,result) {
+            if(err){
+                res.send(err);
+            } else {
+                res.json({
+                    message: "Book updated successfully"
+                });
+            }
+        }
+    );
+});
+
+//DELETE a book
+router.delete('/books/:id', function(req, res){
+    const id = req.params.id;
+
+    db.query("DELETE FROM book WHERE id_book=?",[id],
+        function(err, result){
+            if(err){
+                res.send(err);
+            } else {
+                res.json({
+                    message: "Book deleted successfully"
+                });
+            }
+        }
+    );
+});
+
+
+//GET all borrowers
+router.get('/borrowers', function(req, res){
+    db.query("SELECT * FROM borrower", function(err, result){
+        if(err){
+            res.send(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+//POST, add new borrower
+router.post('/borrowers', function(req, res){
+    const { fname, lname, streetAddress } = req.body;
+    db.query(
+        "INSERT INTO borrower (fname, lname, streetAddress) VALUES (?, ?, ?)", [fname, lname, streetAddress],
+        function(err, result){
+            if(err){
+                res.send(err);
+            } else {
+                res.json({
+                    message: "Borrower added successfully", id: result.insertId
+                });
+            }
+        }
+    );
+});
+
+//UPDATE a book
+router.put('/borrowers/:id', function(req, res){
+    const id = req.params.id;
+    const { fname, lname, streetAddress} = req.body;
+    db.query("UPDATE borrower SET fname =?, lname=?, streetAddress=? WHERE id_borrower=?", [fname, lname, streetAddress, id],
+        function(err,result) {
+            if(err){
+                res.send(err);
+            } else {
+                res.json({
+                    message: "Borrower updated successfully"
+                });
+            }
+        }
+    );
+});
+
+//DELETE a borrower
+router.delete('/borrowers/:id', function(req, res){
+    const id = req.params.id;
+
+    db.query("DELETE FROM borrower WHERE id_borrower=?",[id],
+        function(err, result){
+            if(err){
+                res.send(err);
+            } else {
+                res.json({
+                    message: "Borrower deleted successfully"
+                });
+            }
+        }
+    );
+});
 
 module.exports = router;
